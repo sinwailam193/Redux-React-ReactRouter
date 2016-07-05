c67d4f0b8708cb0643f977e50cb26606b2ca1e55
sinwailam193 <sinwailam193@hotmail.com>
2016-07-04 18:50:05 -0700

    moved passport services around

--- server/router.js
+++ server/router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const passportService = require("./services/passport");
 
 router.use("/api/auth", require("./routers/auth"));
 
--- server/routers/auth.js
+++ server/routers/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jwt-simple");
 const passport = require("passport");
-const passportService = require("../services/passport");
 const User = require("../models/user");
 const config = require("../config");
 
