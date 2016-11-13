d0add5a01f4a6f870cad8bb443d4c55b57e37e26
sinwailam193 <sinwailam193@hotmail.com>
2016-11-13 15:58:04 -0800

    took out eslint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
@@ -9,4 +9,8 @@ import Reducers from './reducers/root.reducer';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
 
-render(<Provider store={createStoreWithMiddleware(Reducers)}><Router history={browserHistory} routes={Routes} /></Provider>, document.getElementById('container'));
+render(
+  <Provider store={createStoreWithMiddleware(Reducers)}>
+    <Router history={browserHistory} routes={Routes} />
+  </Provider>,
+  document.getElementById('container'));
