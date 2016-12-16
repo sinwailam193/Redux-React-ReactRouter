26a3cb61088280b7a949f1167248a7afddd5a404
sinwailam193 <sinwailam193@hotmail.com>
2016-12-16 14:05:45 -0800

    separate our libraries in vendor.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
+const webpack = require("webpack");
+
 module.exports = {
-  entry: [
-    "babel-polyfill",
-    "./src/index.js"
-  ],
+  entry: {
+    app: "./src/index.js",
+    vendor: ["babel-polyfill", "react", "react-dom", "react-router", "redux", "redux-thunk", "react-redux", "axios"]
+  },
   output: {
     path: __dirname + "/public",
     publicPath: "/",
     filename: "bundle.js"
   },
+  plugins: [new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js")],
   module: {
     loaders: [{
       exclude: /node_modules/,
