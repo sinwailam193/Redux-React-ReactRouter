b0f0213c9dcd61e30608e80da6e63050ee709678
sinwailam193 <sinwailam193@hotmail.com>
2016-12-25 10:01:10 -0800

    added redux-devtool

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import ReduxThunk from 'redux-thunk';
 import Routes from './routes';
 import Reducers from './reducers/root.reducer';
 require("./utils");
 
-const store = applyMiddleware(ReduxThunk)(createStore)(Reducers);
+const store = createStore(Reducers, composeWithDevTools(applyMiddleware(ReduxThunk)));
 
 render(
   <Provider store={store}>
