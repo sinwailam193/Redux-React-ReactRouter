949160e482538272d534de9ec4a98a2d35326130
sinwailam193 <sinwailam193@hotmail.com>
2016-12-26 11:45:54 -0800

    set up morgan for environment

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+import fs from "fs";
 import morgan from "morgan";
 import { createServer } from "http";
 import { json } from "body-parser";
@@ -6,8 +8,20 @@ import router from "./router";
 
 const port = process.env.PORT || 3000;
 const app = express();
+const environment = app.get("env");
 
-app.use(morgan("dev"));
+if (environment === "production") {
+  app.use(morgan("common", {
+    skip: (req, res) => res.statusCode < 400,
+    stream: fs.createWriteStream(
+      path.join(__dirname.slice(0, __dirname.indexOf("prod")), "morgan.log"),
+      { flags: "a" }
+    )
+  }));
+} else {
+  app.use(morgan("dev"));
+}
+
 app.use(json({ type: "*/*" }));
 app.use("/", router);
 
