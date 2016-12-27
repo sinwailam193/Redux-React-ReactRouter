1e36947e03f755f10843e7bc99c49bc071d12c0c
sinwailam193 <sinwailam193@hotmail.com>
2016-12-26 19:12:31 -0800

    made change

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import path from "path";
-import fs from "fs";
+import { join } from "path";
+import { createWriteStream } from "fs";
 import morgan from "morgan";
 import { createServer } from "http";
 import { json } from "body-parser";
@@ -13,8 +13,8 @@ const env = app.get("env");
 if (env === "production") {
   app.use(morgan("common", {
     skip: (req, res) => res.statusCode < 400,
-    stream: fs.createWriteStream(
-      path.join(__dirname.slice(0, __dirname.indexOf("prod")), "morgan.log"),
+    stream: createWriteStream(
+      join(__dirname.slice(0, __dirname.indexOf("prod")), "morgan.log"),
       { flags: "a" }
     )
   }));
